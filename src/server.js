f0b68b9944a2ed7c7cff3d383d82b07b4053c343
Hortência Côrtes <hortencia.cortes@gmail.com>
2021-08-23 23:08:08 -0400

    update READAME and GIF

--- src/server.js
+++ src/server.js
@@ -5,13 +5,6 @@ const app = express()
 
 app.use(cors())
 
-app.get('/album', async(req, res) => {
-    //response é a resposta do axios, mas eu tiro o data de dentro do response
-    const { data } = await axios('https://api.deezer.com/album/302127')
-    console.log(res.json(data))
-    
-    return res.json(data)
-})
 app.get('/chart', async(req, res) => {
     //response é a resposta do axios, mas eu tiro o data de dentro do response
     const { data } = await axios('https://api.deezer.com/chart')
